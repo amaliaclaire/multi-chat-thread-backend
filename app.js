fc085227c940d2e78ee127489f6e83607e5bc095
Amalia <amalia.fergin@gmail.com>
2019-05-20 13:53:29 -0700

    server added

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+app.get('/ping', (req, res, next) => {
+  res.json({message: `Pong!!`})
+})
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({error: err})
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({error: {message: 'not found'}})
+})
+
+
+const listener = () => {console.log(`listening on port ${port}`)}
+
+app.listen(port, listener)
