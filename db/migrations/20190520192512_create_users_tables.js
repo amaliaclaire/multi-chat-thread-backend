78ea6cd753f4d02e51215ca21210a5b667f1a7e1
Amalia <amalia.fergin@gmail.com>
2019-05-20 19:54:37 -0700

    migration tables with tables users and tickets

---
+++ db/migrations/20190520192512_create_users_tables.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+    table.string('name').notNullable();
+    table.string('organization').notNullable();
+    table.string('role').notNullable();
+    table.string('reputation').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  })
+  .createTable('Tickets', function(table) {
+    table.increments();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.string('title').notNullable();
+    table.string('description').notNullable();
+    table.integer('category').notNullable();
+    table.date('deadline').notNullable().defaultsTo(false);
+    table.integer('user_id').references('id').inTable('users')
+  })
+};
+
+exports.down = function(knex, Promise) {
+
+};
