0c895cfc323c127278a7a3095d74306f508b3065
Amalia <amalia.fergin@gmail.com>
2019-05-23 19:01:37 -0700

    get all users

---
+++ api/controllers/authControllers.js
@@ -0,0 +1,13 @@
+const model = require("../models/authModels")
+
+
+getUsers = (req, res, next) => {
+  model.getAll(req.body)
+  .then(users => res.status(200).send({users}))
+  .catch(err => console.log(err))
+}
+
+
+
+
+module.exports = { getUsers }
---
+++ api/models/authModels.js
@@ -0,0 +1,14 @@
+const knex = require("../../db/knex")
+const uuid = require("uuid/v4")
+
+getAll = (body) => {
+  return knex("users")
+  .select("*")
+  .where(body)
+  .then(result => result)
+  .catch(err => console.log(err))
+}
+
+
+
+module.exports = { getAll }
